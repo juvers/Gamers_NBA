13c285afdf0673504e11f2494cac1bcd8557dcd3
Jude Kuti <judekuti@gmail.com>
2018-03-02 23:14:42 -0600

    made gamersnba.py dry

--- gamersNBA.py
+++ gamersNBA.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from dbase_setup import Franchise, Base, Player, User
+from dbase_setup import *
 
 engine = create_engine('sqlite:///gamersnba.db')
 
